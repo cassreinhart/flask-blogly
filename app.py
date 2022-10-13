3a68f800962aef550ceedd62e55e54b1d5c4f685
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-10-12 21:19:42 -0500

    added delete route, updated markup to be more functional

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, current_app
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,8 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
-connect_db(app)
-db.create_all()
+# connect_db(app)
+# db.create_all()
 
 app.config['SECRET_KEY'] = "SECRET!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -20,14 +20,13 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def show_home():
     """show home page"""
-    users = User.query.all()
 
-    return render_template('user.html', users=users)
+    return redirect('/users')
 
 @app.route('/users')
 def show_all_users():
     """Show all users"""
-    users = User.query.all();
+    users = User.query.order_by(first_name, last_name).all();
     return render_template('user.html', users=users)
 
 @app.route('/users/new')
@@ -42,7 +41,7 @@ def add_user():
 
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    image_url = request.form['image_url']
+    image_url = request.form['image_url'] or None
 
     user = User(first_name=first_name, last_name=last_name, image_url=image_url)
     db.session.add(user)
@@ -50,9 +49,41 @@ def add_user():
 
     return redirect(f"/users")
 
-@app.route('users/<int: user_id>')
+@app.route('/users/<int:user_id>')
 def show_user_detail():
     """Show user details for a single user"""
 
     user = User.query.get_or_404(user_id)
     return render_template("info.html", user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def show_edit_page():
+    """Show edit page for a user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def post_edited_user():
+    """Process edit form, redirect to Users page"""
+
+    user = User.query.get_or_404(user_id)
+
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user():
+    """Handle form submission to delete user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
--- models.py
+++ models.py
@@ -14,9 +14,9 @@ def connect_db(app):
 class User(db.Model):
     """create a new user"""
 
-    __tablename__ = "user"
-    
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(20), nullable=False)
-    last_name = db.Column(db.String(40), nullable=False)
-    image_url = db.Column(db.String, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnSHPlV1tN5yuFqgNWuEu02d8mxiESub2jGA&usqp=CAU')
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable= False, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnSHPlV1tN5yuFqgNWuEu02d8mxiESub2jGA&usqp=CAU')
